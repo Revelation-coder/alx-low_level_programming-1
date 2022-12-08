b97b03069bb47db9f346eb99bf92bed5abe63e2a
LertonJoine <josinelerton@gmail.com>
2022-12-08 14:58:23 -0800

    number 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints the size of the types
+ * printf - prints the messages
+ * 
+ *Return: return a value
+ */
+int main(void)
+{
+printf("Size of a char: %ld\n", sizeof(char));
+printf("Size of an int: %ld\n", sizeof(int));
+printf("Size of a long int: %ld\n", sizeof(unsigned int));
+printf("Size of a long long int: %ld\n", sizeof(long long int));
+printf("Size of a float: %ld\n", sizeof(float));
+return (0);
+}
