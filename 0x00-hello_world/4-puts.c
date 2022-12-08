933d239cc4b1a6e18fa4123d2c5f8db8d12e257f
LertonJoine <josinelerton@gmail.com>
2022-12-08 11:50:30 -0800

    number 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,7 @@
 
 int main(void)
 {
-  char message[80];
-  message = "\"Programming is like building a multilingual puzzle\n";
+  char message[] = "\"Programming is like building a multilingual puzzle\n";
   puts(message);
   return 0;
 }
