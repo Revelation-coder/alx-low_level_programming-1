9e4e7da2ac01ffb6972c410d2a58649579338ea5
LertonJoine <josinelerton@gmail.com>
2022-12-08 11:34:57 -0800

    number 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
-int main(void){
-  char message[] = "\"Programming is like building a multilingual puzzle";
+int main(void)
+{
+  char message[];
+  message = "\"Programming is like building a multilingual puzzle";
   puts(message);
   return 0;
 }
