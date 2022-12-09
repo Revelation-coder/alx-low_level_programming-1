5585370b4fc91ce22a64d1a5a44b8a19dc23c503
LertonJoine <josinelerton@gmail.com>
2022-12-09 14:25:23 -0800

    number 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
-
+/**
+ * main - prints the alphabet
+ *
+ * Return: return status code
+ */
 int main(void)
 {
-  char alphabet [] = {
-		      'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-		      'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-		      'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
-		      'y', 'z'};
-  int size;
-  size = sizeof(alphabet);
+char letter;
 
- for(int c = 0; c < size; c++)
-    {
-      putchar(alphabet[c]);
-      putchar('\n');
-    };
-  return (0);
-} 
+for (letter = 'a'; letter <= letter; letter++)
+putchar(letter);
+
+putchar('\n');
+return (0);
+}
