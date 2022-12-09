32d8064526a410fb3ce038c94cfa87efd6cdc071
LertonJoine <josinelerton@gmail.com>
2022-12-08 16:25:56 -0800

    number 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+ * main - print the message
+ * write - print the message
+ *
+ * Return: return a status code
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+return (1);
+}
