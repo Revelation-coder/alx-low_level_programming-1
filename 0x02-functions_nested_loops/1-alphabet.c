c8e005c21abaf3627e80a60ff231e14f2a4b212b
LertonJoine <josinelerton@gmail.com>
2022-12-13 05:15:15 -0800

    number 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,12 @@
+void print_message(void);
+void print_alphabet(void);
 #include "main.h"
 /**
  * main - check the code
  * print_alphabet - print the alphabet in the screen
  * Return: return status code
  */
-void print_message(void);
-void print_alphabet(void);
+
 int main(void)
 {
 print_alphabet();
