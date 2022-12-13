38b4333f4cc8c4022332e049f64573b6a99fa363
LertonJoine <josinelerton@gmail.com>
2022-12-13 08:40:25 -0800

    congratulations

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,19 +3,20 @@
 
 /**
  *_isalpha - thels if it is alphabetic
- * Return - retuarn vbalue
+ * @c: takes the char to analyse
+ * Return: return vbalue
  */
 int _isalpha(int c)
 {
-  int value;
+int value;
 
-  if (isalpha(c))
-    {
-      value = 1;
-    }
-  else
-    {
-      value = 0;
-    }
-  return (value);
+if (isalpha(c))
+{
+value = 1;
+}
+else
+{
+value = 0;
+}
+return (value);
 }
