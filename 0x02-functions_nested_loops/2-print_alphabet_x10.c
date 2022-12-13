eced2193607d6f0f7d5b31cded2bf9296ac35b08
LertonJoine <josinelerton@gmail.com>
2022-12-13 05:48:20 -0800

    number 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+void print_alphabet_10x(void)
+{
+  char letter;
+  int count;
+  for (count = 0; count < 10; count++)
+    {
+      for(letter = 'a'; letter <= 'z'; letter++)
+	{
+	  putchar(letter);
+	}
+      putchar('\n');
+    }
+int main(void)
+{
+print_alphabet_10x();
+}
