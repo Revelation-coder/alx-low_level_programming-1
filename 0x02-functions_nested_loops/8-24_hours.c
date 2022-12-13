86c7a17b4c01361b82bc6a595d5e5bd7863b2d1e
LertonJoine <josinelerton@gmail.com>
2022-12-13 13:54:11 -0800

    number 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,19 +11,19 @@ for (minute = 0; minute < 60; minute++)
 {
 if ((hour >= 10) & (minute >= 10))
 {
-printf("%d:%d", hour, minute);
+printf("%d:%d\n", hour, minute);
 }
 else if ((minute >= 10) & (hour < 10))
 {
-printf("0%d:%d", hour, minute);
+printf("0%d:%d\n", hour, minute);
 }
 else if ((minute < 10) & (hour < 10))
 {
-printf("0%d:0%d", hour, minute);
+printf("0%d:0%d\n", hour, minute);
 }
 else if ((hour >= 10) & (minute < 10))
 {
-printf("%d:0%d", hour, minute);
+printf("%d:0%d\n", hour, minute);
 }
 }
 }
