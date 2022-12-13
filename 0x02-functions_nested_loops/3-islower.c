9a6aafd6a5482d97586cea00ed89bda667d14767
LertonJoine <josinelerton@gmail.com>
2022-12-13 07:48:11 -0800

    number 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,15 +3,21 @@
 
 /**
  *_islower - tells if a data is lower or not
+ * he value
+ * Return: returun status code
  */
 int _islower(int c)
-int value = 0;
+
 {
-if(islowr(c){
+int value;
+if (islower(c))
+{
 value = 1;
-}else{
+}
+else
+{
 value = 0;
 }
-}
+
 return (value);
 }
