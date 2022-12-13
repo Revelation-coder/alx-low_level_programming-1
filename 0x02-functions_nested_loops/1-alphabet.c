1623a79c918740152c615847b205e9a0d8cc61ad
LertonJoine <josinelerton@gmail.com>
2022-12-13 06:16:22 -0800

    number 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,16 @@
+#include <stdio.h>
 #include "main.h"
 
 int main(void)
 {
 print_alphabet();
 return (0);
+}
+void print_alphabet()
+{
+  char letter;
+  for (letter = 'a'; letter <= 'z'; letter++){
+    putchar(letter);
+  }
+  putchar('\n');
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,3 @@
 int main(void);
 void print_alphabet_x10(void);
+void print_alphabet(void);
