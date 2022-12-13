b3573766dbd712094d14450b3480d50b2c71956c
LertonJoine <josinelerton@gmail.com>
2022-12-13 02:57:11 -0800

    number 1

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,17 @@
 #include <stdio.h>
 /**
  *print_message - print the message
+ *print_alphabet - will print the alphabet in lowercase
  */
 void print_message(void)
 {
 printf("_putchar\n");
+}
+
+void print_alphabet(void)
+{
+for(char letter = 'a'; letter <= 'z'; letter++)
+putchar(letter);
+
+putchar('\n');
 }
