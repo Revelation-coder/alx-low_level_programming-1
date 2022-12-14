d7211368180acc44d65641ea9c66dd636e579d71
LertonJoine <josinelerton@gmail.com>
2022-12-14 01:29:55 -0800

    number 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,13 +4,16 @@
  * print_last_digit - This program will assign a random number
  * to the variable n each time it is executed
  * @n: the number that will be analysed
- * Return: 0 When successful returns a
+ * Return: last_digit When successful returns a
  */
 
 int print_last_digit(int n)
 {
 int last_digit;
-
+if (n < 0)
+{
+n *= -1;
+}
 last_digit = n % 10;
 _putchar(last_digit + '0');
 
