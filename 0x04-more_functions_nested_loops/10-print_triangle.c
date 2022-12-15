4dcc1169c1dd44386a5a75a118bdd8f7536830c7
LertonJoine <josinelerton@gmail.com>
2022-12-15 11:58:11 -0800

    number 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ *print_triangle - prints a triangle
+ *@size: the size of the triangle
+ */
+void print_triangle(int size)
+{
+int c, l;
+if (size > 0)
+{
+for (l = 0; l < size; l++)
+{
+for (c = 0; c < size; c++)
+{
+if (c >= l)
+{
+_putchar('#');
+}
+else
+{
+_putchara(' ');
+}
+}
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
