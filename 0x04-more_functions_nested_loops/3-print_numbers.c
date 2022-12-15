8369ae53a635430d208ddb96ef8de7a2691cc87d
LertonJoine <josinelerton@gmail.com>
2022-12-15 08:03:57 -0800

    number 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *print_number - print number from 0 - 9
+ */
+
+void print_numbers(void)
+{
+int count;
+
+for (count  = 0; count < 10; c++)
+{
+_putchar(count);
+}
+_putchar('\n');
+}
