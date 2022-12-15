797c2b15c9058aed37112d2fd8e17755ee3b24ce
LertonJoine <josinelerton@gmail.com>
2022-12-15 10:17:25 -0800

    number 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *print_line - prints a line
+ *@n: number of _ that will be printed
+ */
+
+void print_line(int n)
+{
+int count;
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (count = 0; count <= n; count++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
