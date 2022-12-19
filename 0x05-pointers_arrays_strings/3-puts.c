b93f17c0c5ff66a217f582975f6e13289844a159
LertonJoine <josinelerton@gmail.com>
2022-12-19 14:54:55 -0800

    number 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- *
+ * _puts - prints a message in the screen
+ *@str: is the message
  */
 
 void _puts(char *str)
 {
-
-  puts("%s\n",str);
-  
+puts(str);
+puts("\n");
 }
