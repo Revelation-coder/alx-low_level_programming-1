9f5deea592a6a3bae09359670f46715f80f9d2af
LertonJoine <josinelerton@gmail.com>
2023-01-04 09:11:13 -0800

    number 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <math.h>
+/**
+ *_pow_recursion - calculate the pow of x by y
+ * @x: base
+ * @y: exponent
+ * Return: return pow
+ */
+int _pow_recursion(int x, int y)
+{
+return (pow(x, y));
+}
