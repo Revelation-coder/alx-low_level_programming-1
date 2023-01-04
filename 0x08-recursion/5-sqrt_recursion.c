517d5ad22681a727eb5bed0ab4fd914b3433c608
LertonJoine <josinelerton@gmail.com>
2023-01-04 10:52:53 -0800

    number 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <math.h>
+
+/**
+ *_sqrt_recursion - calculate the square root
+ * @n: number
+ * Return: sqaure root
+ */
+int _sqrt_recursion(int n)
+{
+return (sqrt(n));
+
+}
