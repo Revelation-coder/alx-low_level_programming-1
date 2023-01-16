70352fa22355b655acac371a5c25e04e1fbbdff1
Revelation-coder <82636655+Revelation-coder@users.noreply.github.com>
2023-01-16 03:39:50 -0800

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,16 +1,18 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE dog.h
+
 /**
  * struct dog - Define a new struct dog with name, age, owner
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of dog
  **/
+
 struct dog
 {
-	char *name;
-	float age;
-	char *owner;
+char *name;
+float age;
+char *owner;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
