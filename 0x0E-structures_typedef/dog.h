e38020c9d91c2da66c045f1c4e0ca3585f9791fa
LertonJosine <josinelerton@gmail.com>
2023-01-16 03:02:11 -0800

    fix for task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,12 @@
 void init_dog(struct dog *d, char *name, float age, char *owner);
+
+/**
+ * struct dog - Define a new struct dog with name, age, owner
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of dog
+ **/
+
 struct dog
 {
 char *name;
